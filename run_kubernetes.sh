38f7ac9e667109f05ff05e7f6f4699cbac21a3f5
ntepp <ntepp.marcus@gmail.com>
2020-05-20 22:59:16 +0000

    update kubernetes sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,18 +2,19 @@
 
 # This tags and uploads an image to Docker Hub
 
+
 # Step 1:
 # This is your Docker ID/path
 dockerpath=jmntepp/angular-tour-of-heroes
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-sudo /home/ec2-user/bin/kubectl run angular-tour-of-heroes --image=jmntepp/angular-tour-of-heroes --port=80
+/home/ec2-user/bin/kubectl run angular-tour-of-heroes --image=jmntepp/angular-tour-of-heroes --port=80
 
 # Step 3:
 # List kubernetes pods
-sudo /home/ec2-user/bin/kubectl get pods
+/home/ec2-user/bin/kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-sudo /home/ec2-user/bin/kubectl port-forward angular-tour-of-heroes 4200:80
+/home/ec2-user/bin/kubectl port-forward angular-tour-of-heroes 4200:80
