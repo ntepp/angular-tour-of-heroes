3418d8f64b993d3f65debe561dfecc6d2c210300
ntepp <ntepp.marcus@gmail.com>
2020-05-23 11:31:03 +0000

    debug kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,7 +8,7 @@ dockerpath=jmntepp/angular-tour-of-heroes
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-sudo /home/ec2-user/bin/kubectl run angular-tour-of-heroes --image=jmntepp/angular-tour-of-heroes --port=80
+sudo /home/ec2-user/bin/kubectl run --generator=run-pod/v1 angular-tour-of-heroes --image=jmntepp/angular-tour-of-heroes --port=80
 
 # Step 3:
 # List kubernetes pods
@@ -16,4 +16,4 @@ sudo /home/ec2-user/bin/kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-#sudo /home/ec2-user/bin/kubectl angular-tour-of-heroes-78698d85f6-hcs5 4200:80
+sudo /home/ec2-user/bin/kubectl port-forward angular-tour-of-heroes 4200:80
